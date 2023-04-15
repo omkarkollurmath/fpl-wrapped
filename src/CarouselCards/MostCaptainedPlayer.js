1568cd675f41224e70e4ed8ac2650737126ba97b
Aditya Zingade <zingadeaditya24@gmail.com>
2023-04-16 01:30:09 +0530

    add 2,3,4 arrow

--- src/CarouselCards/MostCaptainedPlayer.js
+++ src/CarouselCards/MostCaptainedPlayer.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Jersey from '../Jersey';
 
-export const MostCaptainedPlayer = () => {
+export const MostCaptainedPlayer = (props) => {
 
     //here we will get player and assign team name to this const
     const teamName = 'Aston Villa';
 
     return (
         <React.Fragment>
-            <div>Most Captained Player</div>
+            <div>Most Captained Player:</div>
+            <span>{props.name}</span><br></br>
             <Jersey teamName={teamName} height={200}/>
         </React.Fragment>
       );
