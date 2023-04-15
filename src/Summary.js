effe71608cb95da9eacda80e3a200effd8872ea2
Aditya Zingade <zingadeaditya24@gmail.com>
2023-04-16 00:07:21 +0530

    Added 2,3,4 feature to the frontend and also api

--- src/Summary.js
+++ src/Summary.js
@@ -1,25 +1,55 @@
 import React, { useEffect, useState } from "react";
 import LoadingPage from './LoadingPage';
 import CarouselCards from './CarouselCards/CarouselCards';
+import { useParams } from 'react-router-dom';
 
 export const Summary = () => {
 
-    const [loading, setLoading] = useState(false);
-    const [dataProcessingDone, setDataProcessingDone] = useState(false);
+    const { id } = useParams();
+
+
+    const [loading, setLoading] = useState(true);
+    //const [dataProcessingDone, setDataProcessingDone] = useState(false);
+    const [data, setData] = useState(undefined);
 
     //get all data from api calls 
     //set dataProcessingDone to true once all data processing is done
 
+
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //       const response = await fetch(`http://localhost:8000/get/${id}`);
+    //       const responseData = await response.json();
+    //       setData(responseData);
+    //     };
+      
+    //     fetchData();
+        
+    //     if (data !== undefined) {
+    //       setLoading(false);
+    //     }
+    //   }, [data, id]);
+
     useEffect(() => {
-        if (dataProcessingDone){
-            setLoading(false);
+      const fetchData = async () => {
+        try {
+          const response = await fetch(`http://localhost:8000/get/${id}`);
+          const responseData = await response.json();
+          setData(responseData);
+          setLoading(false);
+        } catch (error) {
+          console.error(error);
         }
-    },[dataProcessingDone]);
+      };
+    
+      fetchData();
+    }, [id]);
+  
 
     return (
         loading ? <LoadingPage /> :
         (
-            <CarouselCards />
+            <CarouselCards data={data}/>
         )
       );
 }
