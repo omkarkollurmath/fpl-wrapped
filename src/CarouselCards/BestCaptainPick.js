6951287ba378ebc6209a163f1c2c8cbcec4c1b80
omkarkollurmath <omkarkollurmath@gmail.com>
2023-04-16 19:23:19 +0530

    1st card and few fixes in 2,3,4 for jersey and GW

--- src/CarouselCards/BestCaptainPick.js
+++ src/CarouselCards/BestCaptainPick.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Jersey from '../Jersey';
 
-export const BestCaptainPick = () => {
+export const BestCaptainPick = (props) => {
 
     //here we will get player and assign team name to this const
-    const teamName = 'Arsenal';
+    const teamName = props.teamName;
 
     return (
         <React.Fragment>
             <div>Best Captain Pick</div>
             <Jersey teamName={teamName} height={200}/>
+            <div>
+                <span>{props.name} (GW{props.gameWeek} - {props.points} points)</span>
+            </div>  
         </React.Fragment>
       );
 }
