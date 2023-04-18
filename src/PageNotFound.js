622bee096e4f95b8ce55185444a59cba387e1089
razor3598 <amdsah42@gmail.com>
2023-04-18 16:23:04 -0400

    Added PageNotFound + about page

--- src/PageNotFound.js
+++ src/PageNotFound.js
@@ -1,10 +1,14 @@
 import React from "react";
+import errorImage from './utils/404.jpg';
 
 const PageNotFound = () => {
     return(
         <div>
-            <h1>404 Page Not Found</h1>
-            <p>We couldn't find the page</p>
+        <br/><br/>
+            <h1>Oops!</h1>
+            <br/>
+            <p>The page you are looking for has been moved, deleted or possibly never existed!</p>
+            <img src={errorImage} alt="404"></img>
         </div>
     )
 }
--- src/about.js
+++ src/about.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import dev3 from './utils/amod.jpg';
   
 const AboutUs = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'Right',
-        alignItems: 'Right',
-        height: '100vh'
-      }}
-    >
-      <h1>Hello World</h1>
-    </div>
-  );
-};
+    <div style={{margin: 45}}>
+
+            <h2>About Us</h2>
+            <br/>
+            <span>We are a group of friends who love building solutions and fun applications in our spare time. This is our first ever project that we are hosting live and
+            we hope that you like it! Feel free to support us from the "Buy me a Coffee" page but do not feel obligied to do so!
+            </span>
+            <br/><br/>
+            <span>We'd appreciate if you share this project with your Fantasy Premier League friends!
+            </span>
+            <br/><br/><br/>
+            <h2>Developers</h2>
+
+            
+        </div>
+  )
+}
   
 export default AboutUs;
