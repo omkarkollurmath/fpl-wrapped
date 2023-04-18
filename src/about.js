89f810fdb297a5e2c03277f8be23e1f8968087e5
razor3598 <amdsah42@gmail.com>
2023-04-18 10:15:21 -0400

    Added LoadingPage + Changed Footer text

---
+++ src/about.js
@@ -0,0 +1,18 @@
+import React from 'react';
+  
+const AboutUs = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'Right',
+        alignItems: 'Right',
+        height: '100vh'
+      }}
+    >
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+  
+export default AboutUs;
