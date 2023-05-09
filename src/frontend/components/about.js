226dd05814504d332d71fc8a0276405fd835c3e3
omkarkollurmath <omkarkollurmath@gmail.com>
2023-05-09 23:09:52 +0530

    add image

--- src/frontend/components/about.js
+++ src/frontend/components/about.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Aditya from './../../utils/amod.jpg'
+import Aditya from './../../utils/aditya.jpg'
 import Amod from './../../utils/amod.jpg'
 import Omkar from './../../utils/omkar.jpg'
 import Instagram from './../../utils/instagram.png'
